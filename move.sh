55274795fdde208a0b5ff5d8b2df2acdf20e9bd6
Emil Ståhl <emil.stahl@icloud.com>
2021-10-14 17:30:46 +0200

    move.sh

---
+++ move.sh
@@ -0,0 +1,8 @@
+sudo cp -r hadoop-2.10.1 /home/emilstahl
+sudo cp -r kafka_2.11-2.0.0 /home/emilstahl
+sudo cp -r spark-2.4.3-bin-hadoop2.7 /home/emilstahl
+sudo cp -r hbase-1.7.1 /home/emilstahl
+
+sudo cp .profile /home/emilstahl
+sudo cp .bashrc /home/emilstahl
+
